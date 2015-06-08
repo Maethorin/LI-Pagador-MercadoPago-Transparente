7879eae119ec574f920dd1023b145c45d028f152
maethorin <maethorin@gmail.com>
2015-06-08 18:16:03 -0300

    Forçando resposta.conteudo na evidência a ser um objeto #36915486813163

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='li-pagador-mercadopago-transparente',
-    version='0.0.12',
+    version='0.0.13',
     url='https://github.com/lojaintegrada/LI-Pagador-MercadoPago-Transparente',
     license='MIT',
     description='Meio de pagamento usando o checkout transparente do MercadoPago (http://developers.mercadopago.com/documentation/custom-checkout-advanced)',
